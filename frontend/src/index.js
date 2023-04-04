c6914b58943ebb33c16687996c657982debb3e5a
abaez8031 <abaez8031@gmail.com>
2023-04-03 21:29:51 -0400

    css reset

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client"
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import "./reset.css"
 import App from './App';
 import configureStore from './store';
 import csrfFetch from './store/csrf';
