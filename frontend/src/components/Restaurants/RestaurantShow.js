1be2766f42089326c8d4c3f70370cfc0fbfa69fa
abaez8031 <abaez8031@gmail.com>
2023-04-06 18:03:35 -0400

    show styling

--- frontend/src/components/Restaurants/RestaurantShow.js
+++ frontend/src/components/Restaurants/RestaurantShow.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getRestaurant } from "../../store/restaurants";
 import "./RestaurantShow.css"
+import bill from "../../assets/icons8-money-bill-32.png"
 
 const RestaurantShow = () => {
   const dispatch = useDispatch();
@@ -16,37 +17,51 @@ const RestaurantShow = () => {
   return (
     <>
     <div className="show-img-container">
-
+      <img src="https://www.travelandleisure.com/thmb/Jw-KKDrA6z1nvDJbr5kyKJSG4vk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/TAL-header-oiji-mi-NEWNYCDINE0223-8c392080598d44dbafdbc87bbcf4d7dd.jpg"></img>
     </div>
+
     <div className="show-rest-left-container">
     <div className="show-rest-info-container">
-    <ol className="show-rest-info-tabs">
-      <li className="show-rest-info-tab">Overview</li>
-      <li>Experiences</li>
-      <li>Offers</li>
-      <li>Popular dishes</li>
-      <li>Photos</li>
-      <li>Menu</li>
-      <li>Reviews</li>
-    </ol>
+      <ol className="show-rest-info-tabs">
+        <li>Overview</li>
+        <li>Experiences</li>
+        <li>Offers</li>
+        <li>Popular dishes</li>
+        <li>Photos</li>
+        <li>Menu</li>
+        <li>Reviews</li>
+      </ol>
     {restaurant && (
       <>
       <h3 className="show-rest-name">{restaurant.name}</h3>
       <div className="rest-overview-header">
-        <div className="rating-info"> * * * * * 5.0</div>
-        <div>Reviews</div>
+
+        <div className="show-star-container">
+          <i className="fa-sharp fa-solid fa-star"></i>
+          <i className="fa-sharp fa-solid fa-star"></i>
+          <i className="fa-sharp fa-solid fa-star"></i>
+          <i className="fa-sharp fa-solid fa-star"></i>
+          <i className="fa-sharp fa-solid fa-star"></i>
+          <span>5.0</span>
+        </div>
+
+        <div className="show-reviews-container">
+        <i class="fa-regular fa-message fa-flip-horizontal"></i>
+          <span>{Math.trunc(Math.random() * 1000 + 1)} Reviews</span>
+        </div>
+        <div className="show-price-container">
+          <img className="show-price-bill" src={bill}></img><span>${Math.trunc(Math.random() * 5 + 1) * 10} and over</span>
+          </div>
+
+        <div className="show-cuisine-container">
         <div>{restaurant.cuisine}</div>
+        </div>
+        
+
       </div>
       </>
     )}
 
-    {restaurant && (<ul>
-      <li>{restaurant.name}</li>
-      <li>{restaurant.phoneNumber}</li>
-      <li>{restaurant.address}</li>
-      <li>{restaurant.cuisine}</li>
-      <li>{restaurant.description}</li>
-    </ul>)}
     </div>
     </div>
 
