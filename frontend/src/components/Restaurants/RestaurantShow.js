c04c6ccee1ac8a30e9921ff87823fb7d8f167c47
abaez8031 <abaez8031@gmail.com>
2023-04-06 22:10:43 -0400

    showpage done for now

--- frontend/src/components/Restaurants/RestaurantShow.js
+++ frontend/src/components/Restaurants/RestaurantShow.js
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 import { getRestaurant } from "../../store/restaurants";
 import "./RestaurantShow.css"
 import bill from "../../assets/icons8-money-bill-32.png"
+import cutlery from "../../assets/icons8-cutlery-100.png"
 
 const RestaurantShow = () => {
   const dispatch = useDispatch();
@@ -21,48 +22,49 @@ const RestaurantShow = () => {
     </div>
 
     <div className="show-rest-left-container">
-    <div className="show-rest-info-container">
-      <ol className="show-rest-info-tabs">
-        <li>Overview</li>
-        <li>Experiences</li>
-        <li>Offers</li>
-        <li>Popular dishes</li>
-        <li>Photos</li>
-        <li>Menu</li>
-        <li>Reviews</li>
-      </ol>
-    {restaurant && (
-      <>
-      <h3 className="show-rest-name">{restaurant.name}</h3>
-      <div className="rest-overview-header">
+      <div className="show-rest-info-container">
+        <ol className="show-rest-info-tabs">
+          <li>Overview</li>
+          <li>Experiences</li>
+          <li>Offers</li>
+          <li>Popular dishes</li>
+          <li>Photos</li>
+          <li>Menu</li>
+          <li>Reviews</li>
+        </ol>
+      {restaurant && (
+        <>
+        <h3 className="show-rest-name">{restaurant.name}</h3>
+          <div className="rest-overview-header">
 
-        <div className="show-star-container">
-          <i className="fa-sharp fa-solid fa-star"></i>
-          <i className="fa-sharp fa-solid fa-star"></i>
-          <i className="fa-sharp fa-solid fa-star"></i>
-          <i className="fa-sharp fa-solid fa-star"></i>
-          <i className="fa-sharp fa-solid fa-star"></i>
-          <span>5.0</span>
+            <div className="show-star-container">
+              <i className="fa-sharp fa-solid fa-star"></i>
+              <i className="fa-sharp fa-solid fa-star"></i>
+              <i className="fa-sharp fa-solid fa-star"></i>
+              <i className="fa-sharp fa-solid fa-star"></i>
+              <i className="fa-sharp fa-solid fa-star"></i>
+              <span>5.0</span>
+            </div>
+
+            <div className="show-reviews-container">
+            <i class="fa-regular fa-message fa-flip-horizontal"></i>
+              <span>{Math.trunc(Math.random() * 1000 + 1)} Reviews</span>
+            </div>
+            <div className="show-price-container">
+              <img className="show-price-bill" src={bill}></img>
+              <span>${Math.trunc(Math.random() * 5 + 1) * 10} and over</span>
+            </div>
+
+            <div className="show-cuisine-container">
+              <img className="show-cuisine-cutlery" src={cutlery}></img>
+              <span>{restaurant.cuisine}</span>
+            </div>
+          
         </div>
-
-        <div className="show-reviews-container">
-        <i class="fa-regular fa-message fa-flip-horizontal"></i>
-          <span>{Math.trunc(Math.random() * 1000 + 1)} Reviews</span>
-        </div>
-        <div className="show-price-container">
-          <img className="show-price-bill" src={bill}></img><span>${Math.trunc(Math.random() * 5 + 1) * 10} and over</span>
-          </div>
-
-        <div className="show-cuisine-container">
-        <div>{restaurant.cuisine}</div>
-        </div>
-        
-
+        </>
+      )}
+        <span className="rest-show-description">{restaurant.description}</span>
       </div>
-      </>
-    )}
-
-    </div>
     </div>
 
     <div className="show-rest-right-container">
