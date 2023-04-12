ee3ef97cc9353be7aced3768dcff7e514cc33ab7
abaez8031 <abaez8031@gmail.com>
2023-04-12 17:21:24 -0400

    searchbar

--- frontend/src/components/ReservationSearch/index.js
+++ frontend/src/components/ReservationSearch/index.js
@@ -3,7 +3,16 @@ import "./ReservationSearch.css"
 const ReservationSearch = () => {
   return (
     <div className="reservation-search">
-      <h3>Find your table for any occasion</h3>
+      <div className="reservation-search-header">
+        <h3>Find your table for any occasion</h3>
+      </div>
+      <div className="search-bar-input">
+        <div className="search-bar">
+          <input className="search-bar" type="text" placeholder="Location, Restaurant or Cuisine"></input>
+        </div>
+        <button className="submit-search-btn">Let's Go</button>
+      </div>
+
     </div>
   )
 }
