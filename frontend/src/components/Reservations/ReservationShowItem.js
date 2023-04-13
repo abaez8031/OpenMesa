8cf621314cb733bd28f92f6367504f868a4a31d1
abaez8031 <abaez8031@gmail.com>
2023-04-13 11:46:48 -0400

    styling

--- frontend/src/components/Reservations/ReservationShowItem.js
+++ frontend/src/components/Reservations/ReservationShowItem.js
@@ -4,6 +4,8 @@ import calendar from "../../assets/icons8-calendar-24.png"
 import { useDispatch } from "react-redux";
 import { deleteReservation } from "../../store/reservations";
 import trash from "../../assets/icons8-trash-can-64.png"
+import clock from "../../assets/icons8-clock-26.png"
+import userIcon from "../../assets/icons8-user-24.png"
 
 const ReservationShowItem = ({reservation}) => {
   const {numOfGuests, time, restaurant, user, date} = reservation
@@ -17,9 +19,9 @@ const ReservationShowItem = ({reservation}) => {
         </div>
         <div className="reservation-show-right">
           <Link to={`/restaurants/${restaurant.id}`}><h3 className="reservation-rest-name"> {restaurant.name}</h3></Link>
-          <p><i className="fa-regular fa-user fa-xl fa-user-res"></i> {numOfGuests}</p>
-          <p><i className="fa-sharp fa-regular fa-clock"></i>{time > 12 ? `${time % 12}` : `${time}`}:00 {time >= 12 ?"PM" : "AM"}</p>
-          <p><img className="reservation-clock" src={calendar}/>{month}-{day}-{year}</p>
+          <div className="num-of-guests-p"><img className="user-icon" src={userIcon}/> <span>{numOfGuests}</span></div>
+          <div className="time-p"><img className="clock-icon" src={clock}/><span>{time > 12 ? `${time % 12}` : `${time}`}:00 {time >= 12 ?"PM" : "AM"}</span></div>
+          <div className="calendar-div"><img className="reservation-calendar" src={calendar}/><span>{month}-{day}-{year}</span></div>
         </div>
 
         <div className="reservation-delete-edit-btns">
