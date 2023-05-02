caea5f38afd2374b2abfc504b725881ef8c95dc6
abaez8031 <abaez8031@gmail.com>
2023-05-02 13:32:34 -0400

    resformstyling

--- frontend/src/components/Reservations/ReservationForm.js
+++ frontend/src/components/Reservations/ReservationForm.js
@@ -16,6 +16,7 @@ const ReservationForm = () => {
   const [numOfGuests, setNumOfGuests] = useState(2);
   const [time, setTime] = useState("")
   let availableReservations = [];
+  console.log(numOfGuests)
   
   const selectTimeBtns = document.querySelectorAll(".select-time-btn")
   selectTimeBtns.forEach(btn => {
@@ -69,38 +70,55 @@ const ReservationForm = () => {
   return (
     <div className="reservation-form-container">
       <form className="reservation-form" onSubmit={handleSubmit}>
-        <h3>Create Reservation</h3>
-        <label>Party Size:
-        <input
-          type="number"
-          min="2"
-          max="20"
-          value={numOfGuests}
-          onChange={(e) => setNumOfGuests(e.target.value)}
-          required
-        />
-        </label>
+        <h3>Make a Reservation</h3>
+        <div className="reservation-info-container">
+          <div >
+            <label for="party-size" id="party-size-label">Party Size</label>
+            <select name="party-size" className="party-size-select" required onChange={(e) => setNumOfGuests(e.target.value)}>
+              <option value="1">1 person</option>
+              <option selected value="2">2 people</option>
+              <option value="3">3 people</option>
+              <option value="4">4 people</option>
+              <option value="5">5 people</option>
+              <option value="6">6 people</option>
+              <option value="7">7 people</option>
+              <option value="8">8 people</option>
+              <option value="9">9 people</option>
+              <option value="10">10 people</option>
+              <option value="11">11 people</option>
+              <option value="12">12 people</option>
+              <option value="13">13 people</option>
+              <option value="14">14 people</option>
+              <option value="15">15 people</option>
+              <option value="16">16 people</option>
+              <option value="17">17 people</option>
+              <option value="18">18 people</option>
+              <option value="19">19 people</option>
+              <option value="20">20 people</option>
+            </select>
+          </div>
 
-        <label>Date:
-        <input
-          type="date"
-          value={date}
-          min={currentDate}
-          onChange={(e) => setDate(e.target.value)}
-          required
-        />
-        </label>
+          <label>Date:
+          <input
+            type="date"
+            value={date}
+            min={currentDate}
+            onChange={(e) => setDate(e.target.value)}
+            required
+          />
+          </label>
 
-        {availableReservations.map((resTime,i) => (
-          <button className="select-time-btn" key={i} value={resTime} onClick={(e) => {
-            e.preventDefault();
-            setTime(e.target.value)
-          }}>{resTime > 12 ? `${resTime % 12}` : `${resTime}`}:00 {resTime >= 12 ?"PM" : "AM"}</button>
-        )
-        )}
-        {availableReservations.length === 0 && <p>No more reservations available today</p>}
+          {availableReservations.map((resTime,i) => (
+            <button className="select-time-btn" key={i} value={resTime} onClick={(e) => {
+              e.preventDefault();
+              setTime(e.target.value)
+            }}>{resTime > 12 ? `${resTime % 12}` : `${resTime}`}:00 {resTime >= 12 ?"PM" : "AM"}</button>
+          )
+          )}
+          {availableReservations.length === 0 && <p>No more reservations available today</p>}
 
-        <button className="make-res-button" type="submit">Make Reservation</button>
+          <button className="make-res-button" type="submit">Make Reservation</button>
+        </div>
       </form>
     </div>
   );
